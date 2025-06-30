22f6b49d155bc3839aaf0ec9893f1e92069c0e98
RaxPlay <121737678+RaxPlay@users.noreply.github.com>
2025-06-29 17:32:47 -0700

    Weather display detail A small detail for the weather display that cahnges it's background color according to the temperature of the city.

--- script.js
+++ script.js
@@ -28,3 +28,25 @@ async function getData() {
         throw new Error("Could not fetch data... Invalid location");
     }
 }
+
+//Function to change weatherDisplay's background and border color according to the temperature of cities.
+function checkTemp(cityTemperature){
+    if(cityTemperature >= 26 && cityTemperature <= 31){
+        weatherDisplay.style.background = "linear-gradient(to bottom right,rgb(76, 43, 193), #A05DD9) no-repeat"
+        weatherDisplay.style.border = "3px solid rgb(174, 36, 220)"
+        let status = "Neutral temperature: ";
+        cityTemperatureDisplay.append(status);
+    }
+    if(cityTemperature >= 10 && cityTemperature <= 25){
+        weatherDisplay.style.background = "linear-gradient(to bottom right,rgb(37, 119, 243), #5DB6D9) no-repeat"
+        weatherDisplay.style.border = "3px solid rgb(33, 147, 218)"
+        let status = "Cold/Cool temperature: ";
+        cityTemperatureDisplay.append(status);
+    }
+    if(cityTemperature > 31){
+        weatherDisplay.style.background = "linear-gradient(to bottom right,rgb(178, 28, 28),rgb(193, 197, 62)) no-repeat"
+        weatherDisplay.style.border = "3px solid rgb(202, 29, 29)"
+        let status = "Hot temperature: ";
+        cityTemperatureDisplay.append(status);
+    }
+}
